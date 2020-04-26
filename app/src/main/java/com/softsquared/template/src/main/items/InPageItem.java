6dd76ccc661fc6c1fe1075efc509e79a2bd37f16
k95yong <37685633+k95yong@users.noreply.github.com>
2020-04-26 17:43:38 +0900

    final

--- app/src/main/java/com/softsquared/template/src/main/items/InPageItem.java
+++ app/src/main/java/com/softsquared/template/src/main/items/InPageItem.java
@@ -3,10 +3,10 @@ package com.softsquared.template.src.main.items;
 public class InPageItem {
     public String a, b, c;
 
-    public InPageItem(String a, String b, String c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public InPageItem(String p_a, String p_b, String p_c) {
+        this.a = p_a;
+        this.b = p_b;
+        this.c = p_c;
     }
 
     public String getA() {
